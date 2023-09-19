e2f90f75630f28f1b82a97159dda4ecd5eba530f
rohit.jadhav1 <rohit.jadhav@raft.ai>
2023-09-19 22:19:27 +0530

    initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+// PascalCassing
+function Message() {
+  // JSX - Javascript XML
+  const name = "Rohit";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
+}
+
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,20 @@
+function ListGroup() {
+  let items = ["New York", "San Francisco", "London", "Tokyo", "Paris"];
+  //   items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
